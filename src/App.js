a547fa24e7d55512db7b2bbfeb32caec602f5d02
Jordan Martinuzzi <jordanpohlman@gmail.com>
2024-12-03 23:51:46 -0700

    add AI functionality

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import List from './components/List';
 import React, { useEffect, useRef, useState } from 'react';
 import Editor from './components/Editor';
 import Quill from 'quill';
-import { TextField, ThemeProvider } from '@mui/material';
+import { TextField, ThemeProvider, Button } from '@mui/material';
 import RelatedItems from './components/RelatedItems';
 import AddSection from './components/AddSection';
 import { createTheme } from '@mui/material/styles';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { createBusinessCasePrompt } from './prompts';
 
 const Delta = Quill.import('delta');
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
 
 const theme = createTheme({
   palette: {
@@ -39,8 +42,29 @@ function App() {
   const [range, setRange] = useState();
   const [lastChange, setLastChange] = useState();
   const [sections, setSections] = useState([]);
+  const [aiResponse, setAiResponse] = useState('');
+
   // Use a ref to access the quill instance directly
   const quillRef = useRef();
+
+  async function aiRun() {
+    const model = genAI.getGenerativeModel({
+      model: "gemini-pro",
+      generationConfig: {
+        temperature: 1.0,
+      },
+    });
+    const prompt = createBusinessCasePrompt;
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    const content = text.split(/```json/i);
+    var filtered = content.filter(function (el) {
+      return el != "";
+    });
+    const json = JSON.parse(filtered[0].replace("```", ""));
+    quillRef.current.setContents(json['ops']);
+  }
 
   const addSectionsToTemplate = (numSections) => {
     var newSection = new Array(numSections).fill("empty");
@@ -58,16 +82,12 @@ function App() {
     <ThemeProvider theme={theme}>
     <div className="App">
       <TextField label="Opportunity" variant="outlined"/>
+      <Button onClick={aiRun}>Generate with AI</Button>
       <Editor
         ref={quillRef}
         readOnly={false}
         defaultValue={new Delta()
-          .insert('Hello')
-          .insert('\n', { header: 1 })
-          .insert('Some ')
-          .insert('initial', { bold: true })
-          .insert(' ')
-          .insert('content', { underline: true })
+          .insert(aiResponse)
           .insert('\n')}
         onSelectionChange={setRange}
         onTextChange={setLastChange}
--- src/components/RelatedItems.js
+++ src/components/RelatedItems.js
@@ -5,7 +5,6 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Card, CardContent, Chip } from '@mui/material';
-import CircularProgress from '@mui/material/CircularProgress';
 
 function RelatedItems() {
     return (
